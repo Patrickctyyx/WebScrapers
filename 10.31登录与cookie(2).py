738c4c136f838363dbb0dcb035354e066683c6ea
Patrick <873948000@qq.com>
2016-11-02 20:30:50 +0800

    11.2 Daily Learn:     Crawling JavaScript content!

--- 10.31登录与cookie(2).py
+++ 10.31登录与cookie(2).py
@@ -3,6 +3,8 @@ import requests
 session = requests.session()
 
 params = {'username': 'Patrick', 'password': 'password'}
+# session对象记录了所有的会话
+# 包括cookie,header，即使变化也会一直捕捉
 s = session.post('http://pythonscraping.com/pages/cookies/welcome.php', params)
 print('Cookie is set to:')
 print(s.cookies.get_dict())
